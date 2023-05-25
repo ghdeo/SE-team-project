003d4d55fb1ff3ee7a592c3f722e3b6657313fbf
seojunP <sjsjwest6@gmail.com>
2023-05-25 15:10:44 +0900

    ApplyInfoCollection 통합

--- collection/ApplyInfoCollection.h
+++ collection/ApplyInfoCollection.h
@@ -1,4 +1,5 @@
-﻿#pragma once
+﻿#ifndef APPLYINFOCOLLECTION_H
+#define APPLYINFOCOLLECTION_H
 
 #include <iostream>
 #include <vector>
@@ -7,14 +8,20 @@
 
 using namespace std;
 
-class ApplyCollection
+class ApplyInfoCollection
 {
 private:
-    vector<pair<string, ApplyInfo>> ownedApplyList;
+    vector<pair<string, ApplyInfo*>> applyInfoList; // 吏?먯옄 ?대쫫, 吏?먯젙蹂?
+    static ApplyInfoCollection* instance; //singleton instance
+    ApplyInfoCollection() {}
+    ApplyInfoCollection(const ApplyInfoCollection& other) {}
 
 public:
-    bool getApplyInfo();                    // 지원 정보를 회사 이름을 기준으로 정렬하여 전부 출력
-    bool deleteCancelApplyInfo(string);     // 지원 정보 삭제
-    bool getApplyStats();                   // 지원한 모든 지원 정보에 대해 지원 업무별 지원 횟수를 출력(지원 정보 통계)
-    bool addApplyList(ApplyInfo);
+    static ApplyInfoCollection* getInstance();  // singleton 객체 instance 반환
+    void addApplyInfo(ApplyInfo* applyInfo);    // 吏?먯젙蹂????    
+    bool getApplyInfo();                        // 지원 정보를 회사 이름을 기준으로 정렬하여 전부 출력
+    bool deleteCancelApplyInfo(string);         // 지원 정보 삭제
+    bool getApplyStats();                       // 지원한 모든 지원 정보에 대해 지원 업무별 지원 횟수를 출력(지원 정보 통계)
 };
+
+#endif
